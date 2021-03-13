1af8f2eff665315ff8a2d8dbd84360da415edcc9
Elvis Baranoski <elvisbaranoski@gmail.com>
2021-03-13 11:59:45 -0300

    Projeto foi criado

---
+++ src/main/java/com/elvisbaranoski/mc/CursoMc1Application.java
@@ -0,0 +1,13 @@
+package com.elvisbaranoski.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/elvisbaranoski/mc/CursoMc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvisbaranoski.mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
