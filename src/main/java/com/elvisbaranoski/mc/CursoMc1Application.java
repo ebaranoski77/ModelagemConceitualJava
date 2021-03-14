40473ab30766486ba1ef8ec669f33ca84302a8cf
Elvis Baranoski <elvisbaranoski@gmail.com>
2021-03-14 14:41:17 -0300

    Criando Instanciação

--- src/main/java/com/elvisbaranoski/mc/CursoMc1Application.java
+++ src/main/java/com/elvisbaranoski/mc/CursoMc1Application.java
@@ -1,13 +1,30 @@
 package com.elvisbaranoski.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMc1Application {
+import com.elvisbaranoski.mc.domain.Categoria;
+import com.elvisbaranoski.mc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoMc1Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMc1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 =new Categoria(null,"INFORMÁTICA");
+		Categoria categoria2 =new Categoria(null,"ESCRITÓRIO");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2));
 	}
 
 }
