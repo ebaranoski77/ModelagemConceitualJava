9be759d07a52eb872affc49723c60acdc7dfe5c9
Elvis Baranoski <elvisbaranoski@gmail.com>
2021-03-14 11:19:08 -0300

    Testando com banco H2 e @Entity criando a base de dados automática!

--- src/main/java/com/elvisbaranoski/mc/domain/Categoria.java
+++ src/main/java/com/elvisbaranoski/mc/domain/Categoria.java
@@ -2,6 +2,11 @@ package com.elvisbaranoski.mc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -15,8 +20,12 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID= 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	
 	private Long id;
 	private String nome;
